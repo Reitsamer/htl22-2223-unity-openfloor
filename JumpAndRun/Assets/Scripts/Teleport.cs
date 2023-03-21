11e8188369e9c191eec98769e45c537eebe414d8
Wolf Reitsamer <reit@htl-donaustadt.at>
2023-03-21 16:34:39 +0100

    .

--- JumpAndRun/Assets/Scripts/Teleport.cs
+++ JumpAndRun/Assets/Scripts/Teleport.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
     private Material material;
+
+    [SerializeField]
+    private float speed = 1f;
 
     private enum State
     {
@@ -32,13 +36,43 @@ public class Teleport : MonoBehaviour
         {
             case State.FadedIn:
                 // Space pressed -> state = State.FadingOut
+                if (Input.GetKeyDown(KeyCode.T))
+                {
+                    state = State.FadingOut;
+                }
                 break;
             case State.FadingIn:
+            {
                 // _Fade = 1 -> state = State.FadedIn
+                float fade = material.GetFloat("_Fade");
+                fade += Time.deltaTime * speed;
+                if (fade >= 1)
+                {
+                    fade = 1;
+                    state = State.FadedIn;
+                }
+
+                material.SetFloat("_Fade", fade);
                 break;
+            }
             case State.FadingOut:
+            {
+                float fade = material.GetFloat("_Fade");
+                fade -= Time.deltaTime * speed;
+                if (fade <= 0)
+                {
+                    fade = 0;
+                    state = State.FadedOut;
+                }
+
+                material.SetFloat("_Fade", fade);
                 break;
+            }
             case State.FadedOut:
+                if (Input.GetKeyDown(KeyCode.T))
+                {
+                    state = State.FadingIn;
+                }
                 break;
             default:
                 break;
